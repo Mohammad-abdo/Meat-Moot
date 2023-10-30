982c1914b6e6fe8a8764b69de56d5ad60e5d6d53
dr-basrawy <137460437+dr-basrawy@users.noreply.github.com>
2023-10-30 20:54:38 +0200

    add adds

---
+++ Models/adds.js
@@ -0,0 +1,27 @@
+const mongoose =require('mongoose');
+
+const addsSchema= new mongoose.Schema({
+    sauces:{
+        type:String,
+        enum:[ "Lemon butter" ,"White sauce" , "Demi glace ",
+         "Honey mustard" , "Dynamite" , "Honey" , "Barbeque sauce"]
+    },
+    salads:{
+        type:String,
+        enum:["Roasted Potato ", "Spicy Potato" , "Mashed Potato" ," Arabic Salad" , "Rocca Salad" , "Turkish Salad" ," Hydria" , "Parsleya"  , 
+            "Cherry Tomato" , "Fried Pepper" , "Roasted Garlic" ]
+  
+    },
+    rice:{
+        type:String,
+        default:"rice"
+    },
+    beverages:{
+        type:String,
+        enum:["Mineral Water" , "Soft Drinks" ]
+    }
+
+})
+
+const adds=mongoose.model("adds",addsSchema)
+module.exports= adds
